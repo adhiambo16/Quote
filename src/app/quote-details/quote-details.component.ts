fd128547b58cf7eaf2be1a1b0703e6d6886bb702
Oluoch Lydia <adhiambolydia96@gmail.com>
2018-05-28 13:34:31 +0300

    Add a quote input decorater

--- src/app/quote-details/quote-details.component.ts
+++ src/app/quote-details/quote-details.component.ts
@@ -1,23 +1,23 @@
-// import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
-// import { Quote } from '../quote';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Quote } from '../quote';
 
-// @Component({
-//   selector: 'app-quote-details',
-//   templateUrl: './quote-details.component.html',
-//   styleUrls: ['./quote-details.component.css']
-// })
-// export class QuoteDetailsComponent implements OnInit {
-//   isComplete: any;
-//   @Input() quote: Quote;
-//   @Output() isDelete = new EventEmitter <boolean>();
+@Component({
+  selector: 'app-quote-details',
+  templateUrl: './quote-details.component.html',
+  styleUrls: ['./quote-details.component.css']
+})
+export class QuoteDetailsComponent implements OnInit {
+  isComplete: any;
+   @Input() quote: Quote;
+  // @Output() isDelete = new EventEmitter <boolean>();
 
-//   quoteDelete(complete: boolean ) {
-//     this.isDelete.emit(complete);
-//   }
-//   constructor() { }
+  // quoteDelete(complete: boolean ) {
+  //   this.isDelete.emit(complete);
+  // }
+  constructor() { }
 
 
-//   ngOnInit() {
+  ngOnInit() {
   }
 
 }
