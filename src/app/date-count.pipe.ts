532fc517f14c81653d6e82c392676957d0ac7924
Oluoch Lydia <adhiambolydia96@gmail.com>
2018-05-28 13:26:54 +0300

    Added a const for seconds in a day

--- src/app/date-count.pipe.ts
+++ src/app/date-count.pipe.ts
@@ -9,7 +9,7 @@ import { Pipe, PipeTransform } from '@angular/core';
     const today:  Date = new Date(); // get current date and time
      const todayWithNoTime: any = new Date(today.getFullYear(), today.getMonth(), today.getDate() );
     const dateDifference = Math.abs(value - todayWithNoTime ); // returns value in milliseconds
-    // const secondsInADay = 86400; // 60 seconds*60 minutes in an hour *24 hours
+    const secondsInADay = 86400; // 60 seconds*60 minutes in an hour *24 hours
 
     // // tslint:disable-next-line:prefer-const
     // const dateDifferenceSeconds =  dateDifference * 0.001; // converts to seconds
