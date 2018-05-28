7831d59a64595b401504ed690f7fba7aebd48975
Oluoch Lydia <adhiambolydia96@gmail.com>
2018-05-28 13:16:54 +0300

    add a function for show description

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,12 +1,12 @@
 export class Quote {
-     //  public upvotes: number;
+      public upvotes: number;
      public downvotes: number;
      public showDescription: boolean;
   
     // tslint:disable-next-line:max-line-length
     constructor(public id: number, public name: string, public description: string, public completeDate: Date, ) {
 
-// this.showDescription = false;
+this.showDescription = false;
 // this.upvotes = 0;
 // this.downvotes = 0;
 // }
