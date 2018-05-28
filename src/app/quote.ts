5dabd5e77110c49218cebcad1009c6fecf2d2024
Oluoch Lydia <adhiambolydia96@gmail.com>
2018-05-28 13:18:32 +0300

    add a function for downvotes

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -2,14 +2,14 @@ export class Quote {
       public upvotes: number;
      public downvotes: number;
      public showDescription: boolean;
-  
+
     // tslint:disable-next-line:max-line-length
     constructor(public id: number, public name: string, public description: string, public completeDate: Date, ) {
 
 this.showDescription = false;
 this.upvotes = 0;
-// this.downvotes = 0;
-// }
+ this.downvotes = 0;
+ }
 
 
 }
