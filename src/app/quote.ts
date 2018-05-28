f1b2774bfe71dcf8922cb6acabb7e5ea0ed2b7d0
Oluoch Lydia <adhiambolydia96@gmail.com>
2018-05-28 13:13:12 +0300

    add  a constructor

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,9 +1,9 @@
-// export class Quote {
-//     public showDescription: boolean;
-//      public upvotes: number;
-//      public downvotes: number;
-//     // tslint:disable-next-line:max-line-length
-//     constructor(public id: number, public name: string, public description: string, public completeDate: Date, ) {
+export class Quote {
+    // public showDescription: boolean;
+    //  public upvotes: number;
+    //  public downvotes: number;
+    // tslint:disable-next-line:max-line-length
+    constructor(public id: number, public name: string, public description: string, public completeDate: Date, ) {
 
 // this.showDescription = false;
 // this.upvotes = 0;
@@ -11,4 +11,4 @@
 // }
 
 
-// }
+}
